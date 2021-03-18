9c08b22783da2d9d1b908f773276b65c3f58c710
masok2008 <masok2008@rambler.ru>
2021-03-18 21:10:42 +0300

    3rd ed

---
+++ Lab1.4/RNG.py
@@ -0,0 +1,30 @@
+# 4th python lab
+
+import random
+import ipaddress
+
+class IPv4RandomNetwork(ipaddress.IPv4Network):
+    def __init__(self):
+        ipaddress.IPv4Network.__init__(self, (random.randint(0x0B000000, 0xDF000000), random.randint(8, 24)), False)
+    def regular(self):
+        return self.is_global
+    def key_value(self):
+        return int(self.prefixlen)*2**32+int(self.network_address)
+
+def netsort(n):
+    return (int(n.netmask), int(n.network_address))
+def gennet(num):
+    setnet =set()
+    while len(setnet) < num :
+        n1=IPv4RandomNetwork()
+        if n1.regular():
+            setnet.add(n1)
+    return list(setnet)
+
+nn = 50
+n1=IPv4RandomNetwork()
+listnet = gennet(nn)
+#for s in listnet: print(s, end=" ")
+#print(" ")
+listnet.sort(key=netsort)
+for s in listnet: print(s)
