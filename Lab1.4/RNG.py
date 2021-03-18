34ee15a9ba15b305e5ce958ea81fe61daf082c94
masok2008 <masok2008@rambler.ru>
2021-03-18 22:50:51 +0300

    3rd ed

--- Lab1.4/RNG.py
+++ Lab1.4/RNG.py
@@ -11,13 +11,13 @@ class IPv4RandomNetwork(ipaddress.IPv4Network):
     def key_value(self):
         return int(self.prefixlen)*2**32+int(self.network_address)
     def key_values(self):
-        return (int(self.netmask), int(self.network_address))
+        return (self.netmask, self.network_address)
 
 def netsort1(n):
     return n.key_values()
 
 def netsort(n):
-    return (int(n.netmask), int(n.network_address))
+    return (n.netmask, n.network_address)
 def gennet(num):
     setnet =set()
     while len(setnet) < num :
@@ -27,9 +27,10 @@ def gennet(num):
     return list(setnet)
 
 nn = 50
-n1=IPv4RandomNetwork()
+#n1=IPv4RandomNetwork()
 listnet = gennet(nn)
 #for s in listnet: print(s, end=" ")
 #print(" ")
-listnet.sort(key=netsort1)
-for s in listnet: print(s)
+#listnet.sort(key=netsort1)
+#for s in listnet: print(s, end=" ")
+for s in sorted(listnet, key=netsort1): print(s)
